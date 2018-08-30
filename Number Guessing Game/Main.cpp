c582ceae50b7139f2d64cc108bc838a6d80971e2
Michael Lafosse <s188037@students.aie.edu>
2018-08-30 12:12:52 -0500

    Works but need to edit

--- Number Guessing Game/Main.cpp
+++ Number Guessing Game/Main.cpp
@@ -1,42 +1,45 @@
 #include <iostream>
 #include <cstdlib>
+#include<time.h>
 
 int main()
 {
+	srand(time(NULL));
 	int rand();
-	int guess;
-	char reply;
-	char input;
+	int randNum;
+	int input = 0;
+	int newNum;
 
-	guess = rand() % 100 + 1;
+	randNum = rand() % 100 + 1;
+	
 
 	std::cout << "Think of a number between 1-100.." << std::endl;
+	
 
-	std::cout << rand() % 100 + 1 << std::endl;
-	std::cout << "Is this your number? Enter 'Y' for yes and 'N' for no." << std::endl;
-	std::cin >> reply;
+	while (true)
+	{
 
-	if (reply == 'Y' || reply == 'y')
-	{
-		reply = true;
-		std::cout << "Thanks for playing" << std::endl;
-	}
-	else if (reply == 'N' || reply == 'n')
-	{
-		reply = false;
-	}
-
-	while (reply == false)
-	{
-		std::cout << "Is your number greater than or less than the guessed number?" << std::endl;
-		std::cout << "Enter 1 for greater than or 2 for less than." << std::endl;
+		std::cout << randNum << std::endl;
+		std::cout << "Is this your number?" << std::endl;
+		std::cout << "Enter 1 for less than, 2 for greater than," << std::endl;
+		std::cout << "3 for the number is correct." << std::endl;
 		std::cin >> input;
-
-		if (input = 1)
 		{
-			std::cout << rand() % 100 + 1 / 2 << std::endl;
+			if (input == 1)
+			{
+				randNum -= 10;
+			}
+			if (input == 2)
+			{
+				randNum += 7;
+			}
+			if (input == 3)
+			{
+				std::cout << "Thanks for playing" << std::endl;
+				break;
+			}
 		}
-
 	}
+	
 	system("pause");
 }
