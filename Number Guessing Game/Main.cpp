a515a79a561de289e55831963ce77c3e197f7669
MichaelLafosse99 <michaellafosse99@gmail.com>
2018-08-30 21:07:16 -0500

    Finished Number Guessing Game

--- Number Guessing Game/Main.cpp
+++ Number Guessing Game/Main.cpp
@@ -8,6 +8,10 @@ int main()
 	int rand();
 	int randNum;
 	int input = 0;
+	const int min = 1;
+	const int max = 100;
+	int prevMinNum = 0;
+	int prevMaxNum = 0;
 
 	randNum = rand() % 100 + 1;
 	
@@ -17,20 +21,36 @@ int main()
 
 	while (true)
 	{
-
 		std::cout << randNum << std::endl;
+		if (randNum > max || randNum < min)
+		{
+			std::cout << "Sorry I guessed a number that wasn't between " << min << " and " << max << std::endl;
+		}
 		std::cout << "Is this your number?" << std::endl;
 		std::cout << "Enter 1 for less than, 2 for greater than," << std::endl;
 		std::cout << "3 for the number is correct." << std::endl;
 		std::cin >> input;
+
 		{
 			if (input == 1)
 			{
-				randNum -= 10;
+				prevMaxNum = randNum;
+
+				randNum = rand() % (prevMaxNum - prevMinNum) + prevMinNum;
+				if (randNum == prevMinNum)
+				{
+					randNum++;
+				}
 			}
-			if (input == 2)
+			else if (input == 2)
 			{
-				randNum += 7;
+				prevMinNum = randNum;
+				
+				randNum = rand() % (prevMaxNum - prevMinNum) + prevMinNum;
+				if (randNum == prevMinNum)
+				{
+					randNum++;
+				}
 			}
 			if (input == 3)
 			{
